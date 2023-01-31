92ad90943709e34b4ee8d3380134d3a0c6b75552
Marcus Huang <marcus65001@gmail.com>
2023-01-31 11:19:58 -0700

    bag

--- packages/my_package/src/odometry_node.py
+++ packages/my_package/src/odometry_node.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+import math
+
 import numpy as np
 import os
 import rospy
@@ -24,6 +26,11 @@ class OdometryNode(DTROS):
 
         # bag
         self.bag=rosbag.Bag("/data/bags/encoder.bag", "w")
+        self.bag_flag=True
+
+        # distance counter
+        self.dist_l = 0
+        self.dist_r = 0
 
         # Subscribing to the wheel encoders
         self.sub_encoder_ticks_left = rospy.Subscriber("~tick_l", WheelEncoderStamped, self.cb_enc_l)
@@ -37,27 +44,48 @@ class OdometryNode(DTROS):
         self.log("Initialized")
 
     def cb_enc_l(self,msg):
-        self.cb_encoder_data("l",msg)
+        if self.bag_flag:
+            self.cb_encoder_data("l",msg)
     def cb_enc_r(self, msg):
-        self.cb_encoder_data("r",msg)
+        if self.bag_flag:
+            self.cb_encoder_data("r",msg)
     def cb_encoder_data(self, wheel, msg):
         """ Update encoder distance information from ticks.
         """
-        if wheel=="l":
-            ltick=Int32()
-            ltick.data=msg.data
-            self.bag.write("tick_l",ltick)
+        diff = np.pi * 0.066 * (msg.data / 135)
+        mtick=Int32()
+        mtick.data=msg.data
+        mdiff=Float32()
+        mdiff.data=diff
+        mdist = Float32()
+        if wheel=='l':
+            self.dist_l+=diff
+            mdist.data = self.dist_l
+            self.pub_integrated_distance_left.publish(mdist)
         else:
-            rtick = Int32()
-            rtick.data = msg.data
-            self.bag.write("tick_r", rtick)
+            self.dist_r+=diff
+            mdist.data = self.dist_r
+            self.pub_integrated_distance_right.publish(mdist)
+
+        if self.bag_flag:
+            self.bag.write("tick_{}".format(wheel), mtick)
+            self.bag.write("diff_{}".format(wheel), mdiff)
+            self.bag.write("dist_{}".format(wheel), mdiff)
 
     def cb_executed_commands(self, msg):
         """ Use the executed commands to determine the direction of travel of each wheel.
         """
-        self.bag.write("wheels_cmd_executed", msg)
+        if self.bag_flag:
+            self.bag.write("wheels_cmd_executed", msg)
 
     def on_shutdown(self):
+        mdistl=Float32()
+        mdistl.data=self.dist_l
+        mdistr = Float32()
+        mdistr.data = self.dist_r
+        self.pub_integrated_distance_left.publish(mdistl)
+        self.pub_integrated_distance_right.publish(mdistr)
+        self.bag_flag=False
         self.bag.close()
 
 
